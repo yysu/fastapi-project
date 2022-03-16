b19c7a4346e2ac653bba7f35dc9c4ed8a8d93096
YYSU <jim123820@gmail.com>
2022-03-16 23:37:25 +0800

    change api to rest api

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 class Post(BaseModel):
     title: str
     content: str
+    published: bool = True
+    rating: Optional[int] = None
+
 
 @app.get("/")
 async def root():
@@ -16,7 +20,7 @@ async def root():
 def get_posts():
     return {"data": "This is your post"}
 
-@app.post('/createposts')
-def create_posts(new_post: Post):
-    print(new_post)
-    return {"data": "new_post"}
+@app.post('/posts')
+def create_posts(post: Post):
+    print(post)
+    return {"data": post.dict()}
