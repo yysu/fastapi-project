6decbfeb2554fd11ca40bd330715f02bed9cff6c
YYSU <jim123820@gmail.com>
2022-03-18 00:09:29 +0800

    Add HTTPException

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -20,7 +20,7 @@ def find_post(id):
     for p in my_post:
         if p["id"] == id:
             return p
-    return "None"
+    return None
 
 @app.get("/")
 async def root():
@@ -38,6 +38,9 @@ def create_posts(post: Post):
     return {"data": post_dict}
 
 @app.get('/posts/{id}')
-def get_post(id: int):
+def get_post(id: int, response: Response):
     post = find_post(int(id))
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, \
+            detail=f"post with id: {id} was not found")
     return {"data": f"{post}"}
