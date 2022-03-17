94c7db113d81cd56fbb04e4a54a68cdb3d930689
YYSU <jim123820@gmail.com>
2022-03-18 00:16:51 +0800

    Add HTTP code 204 when delete succeed

--- main.py
+++ main.py
@@ -12,14 +12,20 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
-my_post = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, \
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, \
         {"title": "title of post 2", "content": "content of post 2", "id": 2}]
 
 
 def find_post(id):
-    for p in my_post:
+    for p in my_posts:
         if p["id"] == id:
             return p
+    return None
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
     return None
 
 @app.get("/")
@@ -28,13 +34,13 @@ async def root():
  
 @app.get('/posts')
 def get_posts():
-    return {"data": my_post}
+    return {"data": my_posts}
 
 @app.post('/posts', status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 1000000000)
-    my_post.append(post_dict)
+    my_posts.append(post_dict)
     return {"data": post_dict}
 
 @app.get('/posts/{id}')
@@ -44,3 +50,11 @@ def get_post(id: int, response: Response):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, \
             detail=f"post with id: {id} was not found")
     return {"data": f"{post}"}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    #deleting post
+    # find the index in the array that has required ID
+    index = find_index_post(id)
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
