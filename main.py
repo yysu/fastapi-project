fa7091e8124d0051e63db77ae81be821a9ae0df0
YYSU <jim123820@gmail.com>
2022-03-19 21:01:17 +0800

    Add updated method

--- main.py
+++ main.py
@@ -61,3 +61,15 @@ def delete_post(id: int):
             {id} doesn\'t exist')
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id : \
+            {id} doesn\'t exist')
+    
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {'data': post_dict}
