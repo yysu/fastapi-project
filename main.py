bd366830750d9194e57098d2cf57b1971e0db69f
YYSU <jim123820@gmail.com>
2022-03-19 20:51:49 +0800

    Add error exception when delete

--- main.py
+++ main.py
@@ -56,5 +56,8 @@ def delete_post(id: int):
     #deleting post
     # find the index in the array that has required ID
     index = find_index_post(id)
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id : \
+            {id} doesn\'t exist')
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
