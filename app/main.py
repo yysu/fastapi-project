867c2a8519cbebf26889f60cca8a97c7ff2373fb
YYSU <jim123820@gmail.com>
2022-04-03 23:26:55 +0800

    Remove data in response

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ async def root():
 @app.get('/posts')
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
 @app.post('/posts', status_code=status.HTTP_201_CREATED)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
@@ -43,7 +43,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data": new_post}
+    return new_post
 
 @app.get('/posts/{id}')
 def get_post(id: int, response: Response, db: Session = Depends(get_db)):
@@ -53,7 +53,7 @@ def get_post(id: int, response: Response, db: Session = Depends(get_db)):
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, \
             detail=f"post with id: {id} was not found")
-    return {"data": post}
+    return post
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
@@ -77,4 +77,4 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
 
-    return {'data': post_query.first()}
+    return post_query.first()
