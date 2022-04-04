cf46ac64d28fcde35a43712fb07ab70087dca5ed
YYSU <jim123820@gmail.com>
2022-04-04 14:03:30 +0800

    Add some user function

--- app/main.py
+++ app/main.py
@@ -7,10 +7,8 @@ from random import randrange
 from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import Session
 from typing import List
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
-
-
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -82,9 +80,22 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
 
 @app.post('/users', status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    # hash the password
+    user.password = utils.hash(user.password)
+
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
     return new_user
+
+
+@app.get('/users/{id}', response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, \
+            detail=f"user with id: {id} was not found")
+    return user
