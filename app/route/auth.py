680d97b0f49c16f7dfdfef0c314ebc3f09ca2814
YYSU <jim123820@gmail.com>
2022-04-17 01:35:11 +0800

    remove unnecessary import

--- app/route/auth.py
+++ app/route/auth.py
@@ -1,4 +1,3 @@
-from turtle import mode
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
